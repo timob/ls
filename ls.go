63033d595531d7a68706919e658cc93e83041362
Tim O'Brien <tim@tekao.net>
2015-10-08 06:10:23 +0100

    add help string

--- ls.go
+++ ls.go
@@ -408,6 +408,9 @@ func main() {
 			recursiveList = true
 		case "-O":
 			onlyHidden = true
+		case "--help":
+			fmt.Print(helpStr)
+			os.Exit(0)
 		default:
 			log.Fatalf("unkown option %s", options.Data[iter.Pos()])
 		}
@@ -519,3 +522,20 @@ func main() {
 		display(selected.Data, "")
 	}
 }
+
+var helpStr =
+`Usage: ls [OPTION]... [FILE]...
+List information about the FILEs (the current directory by default).
+Sort entries alphabetically unless a sort option is given.
+	-a					do not ignore entries starting with .
+	-A					do not list implied . and ..
+	-d					list directory entries instead of contents
+	-t					sort by modification time, newest first
+	-S					sort by file size
+	-r					reverse order while sorting
+	-l					use a long listing format
+	-h					with -l, print sizes in human readable format
+	-R					list subdirectories recursively, sorting all files
+	-O					only list entries starting with .
+	--help				display this help and exit
+`
