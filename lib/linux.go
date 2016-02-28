7dc5fd3042e09f9c53172f9a87abb9e1e27e1994
Tim O'Brien <tim@tekao.net>
2016-02-28 14:36:27 +0000

    fix username lookup, add groupname lookup for linux

--- lib/linux.go
+++ lib/linux.go
@@ -8,7 +8,86 @@ import (
 	"os/user"
 	"syscall"
 	"unsafe"
+	"runtime"
+	"strconv"
 )
+
+/*
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <stdlib.h>
+
+static int mygetgrgid_r(int gid, struct group *grp,
+	char *buf, size_t buflen, struct group **result) {
+	return getgrgid_r(gid, grp, buf, buflen, result);
+}
+*/
+import "C"
+
+type UnknownGroupIdError int
+
+func (e UnknownGroupIdError) Error() string {
+	return "user: unknown group id " + strconv.Itoa(int(e))
+}
+
+// taken from os/user/lookup_unix.go
+func groupNameOSLookup(gid int) (string, error) {
+	var grp C.struct_group
+	var result *C.struct_group
+
+	var bufSize C.long
+	if runtime.GOOS == "dragonfly" || runtime.GOOS == "freebsd" {
+		// DragonFly and FreeBSD do not have _SC_GETPW_R_SIZE_MAX
+		// and just return -1.  So just use the same
+		// size that Linux returns.
+		bufSize = 1024
+	} else {
+		bufSize = C.sysconf(C._SC_GETGR_R_SIZE_MAX)
+		if bufSize <= 0 || bufSize > 1<<20 {
+			return "", fmt.Errorf("user: unreasonable _SC_GETGR_R_SIZE_MAX of %d", bufSize)
+		}
+	}
+	buf := C.malloc(C.size_t(bufSize))
+	defer C.free(buf)
+	var rv C.int
+	// mygetgrgid_r is a wrapper around getgrgid_r to
+	// to avoid using gid_t because C.gid_t(uid) for
+	// unknown reasons doesn't work on linux.
+	rv = C.mygetgrgid_r(C.int(gid),
+	&grp,
+	(*C.char)(buf),
+	C.size_t(bufSize),
+	&result)
+	if rv != 0 {
+		return "", fmt.Errorf("ls: lookup group failed id %d: %s", gid, syscall.Errno(rv))
+	}
+	if result == nil {
+		return "", UnknownGroupIdError(gid)
+	}
+	return C.GoString(grp.gr_name), nil
+}
+
+
+var groupLookupCache = make(map[string]string)
+
+func groupLookup(id string) (string, error) {
+	if v, ok := groupLookupCache[id]; ok {
+		return v, nil
+	} else {
+		i, e := strconv.Atoi(id)
+		if e != nil {
+			return "", e
+		}
+		g, err := groupNameOSLookup(i)
+		if err == nil {
+			groupLookupCache[id] = g
+			return g, nil
+		}
+		return "", err
+	}
+}
 
 var userLookupCache = make(map[string]string)
 
@@ -18,7 +97,7 @@ func userLookUp(id string) (string, error) {
 	} else {
 		u, err := user.LookupId(id)
 		if err == nil {
-			userLookupCache[id] = u.Name
+			userLookupCache[id] = u.Username
 			return u.Name, nil
 		}
 		return "", err
@@ -55,7 +134,7 @@ func GetLongInfo(info os.FileInfo) *LongInfo {
 		userName = u
 	}
 	group := fmt.Sprintf("%d", stat.Gid)
-	if g, err := userLookUp(group); err == nil {
+	if g, err := groupLookup(group); err == nil {
 		group = g
 	}
 	return &LongInfo{userName, group, int(stat.Nlink)}
